6803018b217592b581ab1dab19725dcb8b9492bb
Salik Hussaini <47673489+salikhussaini@users.noreply.github.com>
2024-01-15 11:22:49 -0600

    Update and rename py.py to app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from tkinter import filedialog, messagebox, ttk    # Import messagebox from tkin
 from tkinter import filedialog
 import sqlite3
 import pandas as pd
+import pyodbc
 
 class CSVToSQLiteConverter:
     def __init__(self, root):
@@ -11,13 +12,29 @@ class CSVToSQLiteConverter:
         self.root = root
         self.root.title("CSV to SQLite Converter") # Set the window title
         self.df = None
+
+        self.TD_UserName = None
+        self.TD_PassWord = None
         # Initialize variables for progress bar
         self.progress_var = tk.DoubleVar()
         self.progress_bar = ttk.Progressbar(self.root, variable=self.progress_var, mode="indeterminate")
 
         # Create the GUI widgets
         self.create_widgets()
+        # Bind the callback function to the window close event
+        root.protocol("WM_DELETE_WINDOW", self.on_close)
+    def on_close(self):
+        # Ask for confirmation before closing
+        confirmed = messagebox.askokcancel("Confirmation", "Do you want to close the application?")
+        
+        # This function will be called when the window is closed
+        if confirmed:
+            # Close the ODBC connection if it exists
+            if self.odbc_connection is not None:
+                self.odbc_connection.close()
 
+            # Destroy the root window
+            self.root.destroy()
     def create_widgets(self):
         # Create labels and buttons
         self.label = tk.Label(self.root, text="Select CSV File:")
@@ -46,6 +63,9 @@ class CSVToSQLiteConverter:
 
         self.preview_button = tk.Button(self.operations_frame, text="Preview Data", command=self.preview_data)
         self.preview_button.pack(side=tk.LEFT, padx=5)
+
+        self.teradata_button = tk.Button(self.operations_frame, text="Connect to TeraData", command=self.get_teradata_credentials)
+        self.teradata_button.pack(side=tk.LEFT, padx=5)
         
         self.status_bar = tk.Label(self.root, text="Ready", bd=1, relief=tk.SUNKEN, anchor=tk.W)
         self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
@@ -181,6 +201,56 @@ class CSVToSQLiteConverter:
         preview_text = tk.Text(preview_window, wrap=tk.WORD)
         preview_text.insert(tk.END, self.df.head())
         preview_text.pack()
+    def get_teradata_credentials(self):
+        # Create a new Toplevel window for getting Teradata credentials
+        credentials_window = tk.Toplevel(self.root)
+        credentials_window.title("Teradata Credentials")
+
+        # Create a status bar for the credentials window
+        status_bar_credentials = tk.Label(credentials_window, text="Ready", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        status_bar_credentials.grid(row=3, column=0, columnspan=2, sticky=tk.W + tk.E)
+
+
+        # Create labels and entry widgets for username and password
+        tk.Label(credentials_window, text="Username:").grid(row=0, column=0, padx=10, pady=5)
+        username_entry = tk.Entry(credentials_window)
+        username_entry.grid(row=0, column=1, padx=10, pady=5)
+
+        tk.Label(credentials_window, text="Password:").grid(row=1, column=0, padx=10, pady=5)
+        password_entry = tk.Entry(credentials_window, show='*')
+        password_entry.grid(row=1, column=1, padx=10, pady=5)
+        teradata_username = None
+        
+        # Function to get credentials when OK button is pressed
+        def get_credentials():
+            self.TD_UserName = username_entry.get()
+            self.TD_PassWord = password_entry.get()
+            if self.TD_UserName is '' or self.TD_PassWord is '':
+                self.show_message("Warning", "UserName or PassWord Not Entered", "warning")
+                return
+            else:
+                status_bar_credentials.config(text="Credentials entered")
+                    
+        def connect_to_teradata():
+            if self.TD_UserName is not None and self.TD_PassWord is not None:
+                try:
+                    # Construct the ODBC connection string
+                    odbc_connection_string = f"DSN={teradata_dsn};UID={self.TD_UserName};PWD={self.TD_PassWord}"
+
+                    # Establish ODBC connection to Teradata
+                    conn = pyodbc.connect(odbc_connection_string)
+                    status_bar_credentials.config(text="Connected to Teradata")
+                    self.conn = conn
+                except pyodbc.Error as e:
+                    status_bar_credentials.config(text=f"Error connecting to Teradata: {e}")
+                    return None
+        
+        # OK button to get credentials
+        ok_button = tk.Button(credentials_window, text="OK", command=get_credentials)
+        ok_button.grid(row=2, column=0, columnspan=1, pady=10)
+        ok_button = tk.Button(credentials_window, text="Connect to TeraData", command=connect_to_teradata)
+        ok_button.grid(row=2, column=1, columnspan=1, pady=10)
+
     def get_file_path(self, file_type, file_extension, save=False):
         dialog_method = filedialog.asksaveasfilename if save else filedialog.askopenfilename
         file_path = dialog_method(filetypes=[(file_type, file_extension)])
